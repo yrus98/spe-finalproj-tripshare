e875446fd05e2a6791e8d5f174e15e4b95352d86
yrus <shahrushi199891@gmail.com>
2021-04-17 21:59:24 +0530

    Changed http config

--- src/main/java/com/speproject/tripshare/config/SecurityConfiguration.java
+++ src/main/java/com/speproject/tripshare/config/SecurityConfiguration.java
@@ -9,9 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.speproject.tripshare.service.UserService;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 @Configuration
 @EnableWebSecurity
@@ -40,23 +43,43 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(
-				 "/registration**",
+			http.csrf().disable()
+				.requestCache().disable() // do not preserve original request before redirecting to login page as we will return status code instead of redirect to login page (this is important to disable otherwise session will be created on every request (not containing sessionId/authToken) to non existing endpoint aka curl -i -X GET 'http://localhost:8080/unknown')
+				.authorizeRequests()
+				.antMatchers("/registration**",
 	                "/js/**",
 	                "/css/**",
 	                "/img/**").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/login")
-		.permitAll()
-		.and()
-		.logout()
-		.invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutSuccessUrl("/login?logout")
-		.permitAll();
+				.anyRequest().hasRole("USER").and()
+				.exceptionHandling()
+				.accessDeniedHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN)) // if someone tries to access protected resource but doesn't have enough permissions
+				.authenticationEntryPoint((req, resp, ex) -> resp.setStatus(SC_UNAUTHORIZED)).and() // if someone tries to access protected resource without being authenticated (LoginUrlAuthenticationEntryPoint used by default)
+				.formLogin()
+				.loginProcessingUrl("/login") // authentication url
+				.successHandler((req, resp, auth) -> resp.setStatus(SC_OK)) // success authentication
+				.failureHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN)).and() // bad credentials
+				.sessionManagement()
+				.invalidSessionStrategy((req, resp) -> resp.setStatus(SC_UNAUTHORIZED)).and() // if user provided expired session id
+				.logout()
+				.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()); // return status code on logout
+
+//		http.csrf().disable().authorizeRequests().antMatchers(
+//				 "/registration**",
+//	                "/js/**",
+//	                "/css/**",
+//	                "/img/**").permitAll()
+//		.anyRequest().authenticated()
+//		.and()
+//		.formLogin()
+//		.loginPage("/login")
+//		.permitAll()
+//		.and()
+//		.logout()
+//		.invalidateHttpSession(true)
+//		.clearAuthentication(true)
+//		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//		.logoutSuccessUrl("/login?logout")
+//		.permitAll();
 	}
 
 }

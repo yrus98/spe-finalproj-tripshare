3910f88d5b0b286a70f7bed0c12fd8e4cf5c0d87
yrus <shahrushi199891@gmail.com>
2021-04-21 10:50:38 +0530

    Added Duplicate email message on registration

--- src/main/java/com/speproject/tripshare/web/UserRegistrationController.java
+++ src/main/java/com/speproject/tripshare/web/UserRegistrationController.java
@@ -1,6 +1,8 @@
 package com.speproject.tripshare.web;
 
 import com.speproject.tripshare.web.dto.UserRegistrationDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,9 +33,14 @@ public class UserRegistrationController {
 	}
 	
 	@PostMapping
-	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
-		userService.save(registrationDto);
-		return "redirect:/registration?success";
+	public ResponseEntity registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		try {
+			userService.save(registrationDto);
+			return ResponseEntity.status(HttpStatus.OK).body("{'data':'Registered Successfully'}");
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{'data':'Email already in use'}");
+//			return "redirect:/registration?success";
+		}
 	}
 
 }
